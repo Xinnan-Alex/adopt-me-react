14730aadf3d80f5b00e90cc20968f6d4071e0f30
Xinnan.Alex <leongxinnan@outlook.my>
2024-03-17 22:14:55 +0800

    added in code-splitting to improve performance and reduce size

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import React, { useState } from "react";
+import React, { lazy, Suspense, useState } from "react";
 import { createRoot } from "react-dom/client";
-import SearchParams from "./SearchParams";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Details from "./Details";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AdoptedPetContext from "./AdoptedPetContext";
 
@@ -14,6 +12,10 @@ const queryClient = new QueryClient({
     },
   },
 });
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
+
 const App = () => {
   const adoptedPet = useState(null);
   return (
@@ -21,13 +23,21 @@ const App = () => {
       <BrowserRouter>
         <AdoptedPetContext.Provider value={adoptedPet}>
           <QueryClientProvider client={queryClient}>
-            <header>
-              <Link to={"/"}>Adopt Me!</Link>
-            </header>
-            <Routes>
-              <Route path="/details/:id" element={<Details />} />
-              <Route path="/" element={<SearchParams />} />
-            </Routes>
+            <Suspense
+              fallback={
+                <div className="loading-pane">
+                  <h2 className="loader">🌀</h2>
+                </div>
+              }
+            >
+              <header>
+                <Link to={"/"}>Adopt Me!</Link>
+              </header>
+              <Routes>
+                <Route path="/details/:id" element={<Details />} />
+                <Route path="/" element={<SearchParams />} />
+              </Routes>
+            </Suspense>
           </QueryClientProvider>
         </AdoptedPetContext.Provider>
       </BrowserRouter>
